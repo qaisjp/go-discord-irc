qaisjp/go-discord-irc

commit b3f18738c351c9bc3a217c6989d1f6a4d93686ec

Refactor/rename IRCToMarkdown to BlocksToMarkdown
Some logic change was needed inside markdown.go to prevent modification
of blocks.

irc_listener.go also now uses ircf.StripColor, which should no longer
munch on commas.

irc/format/markdown.go
@@ -2,30 +2,22 @@ package ircf
 
 // From https://github.com/reactiflux/discord-irc/blob/87a3458bdde48290960405f2bf0cf53b7ff17b5e/lib/formatting.js#L25
 
-func IRCToMarkdown(text string) string {
-	blocks := Parse(text)
-	for i, b := range blocks {
-		// Consider reverse as italic, some IRC clients use that
-		if b.Reverse {
-			blocks[i].Italic = true
-		}
-	}
-
+func BlocksToMarkdown(blocks []Block) string {
 	mdText := ""
 
-	for i := 0; i <= len(blocks); i++ {
+	for i, block := range blocks {
 		// Default to unstyled blocks when index out of range
-		var block Block
-		if i < len(blocks) {
-			block = blocks[i]
-		}
-		var prevBlock Block
+		prevBlock := Empty
 		if i > 0 {
 			prevBlock = blocks[i-1]
 		}
 
+		// Consider reverse as italic, some IRC clients use that
+		prevItalic := prevBlock.Italic || prevBlock.Reverse
+		italic := block.Italic || block.Reverse
+
 		// Add start markers when style turns from false to true
-		if !prevBlock.Italic && block.Italic {
+		if !prevItalic && italic {
 			mdText += "*"
 		}
 		if !prevBlock.Bold && block.Bold {
@@ -43,7 +35,7 @@ func IRCToMarkdown(text string) string {
 		if prevBlock.Bold && !block.Bold {
 			mdText += "**"
 		}
-		if prevBlock.Italic && !block.Italic {
+		if prevItalic && !italic {
 			mdText += "*"
 		}
 

irc/format/format_test.go
@@ -43,7 +43,7 @@ func TestAllBlocks(t *testing.T) {
 func TestMarkdown(t *testing.T) {
 	assert.Equal(t,
 		`Hello, **Wor*ld*!** My name is __First__ Last. Testing reset***__ONETWO__***THREE. And *reverse*!`,
-		IRCToMarkdown(msgWithoutColor),
+		BlocksToMarkdown(Parse(msgWithoutColor)),
 	)
 }
 

bridge/irc_listener.go
@@ -1,16 +1,12 @@
 package bridge
 
 import (
-	"regexp"
 	"strings"
 
-	"github.com/qaisjp/go-discord-irc/irc/format"
-	"github.com/qaisjp/go-ircevent"
+	ircf "github.com/qaisjp/go-discord-irc/irc/format"
+	irc "github.com/qaisjp/go-ircevent"
 	log "github.com/sirupsen/logrus"
 )
-
-//LineFrom https://github.com/fent/irc-colors.js/blob/ec113439110aad2e7ecd653095d46f99c1d417fc/lib/irc-colors.js#L104
-var colorRegex = regexp.MustCompile(`\x03\d{0,2}(,\d{0,2}|\x02\x02)?`)
 
 type ircListener struct {
 	*irc.Connection
@@ -115,7 +111,7 @@ func (i *ircListener) OnPrivateMessage(e *irc.Event) {
 		msg = "_" + msg + "_"
 	}
 
-	msg = ircf.IRCToMarkdown(colorRegex.ReplaceAllString(msg, ""))
+	msg = ircf.BlocksToMarkdown(ircf.Parse(ircf.StripColor(msg)))
 
 	go func(e *irc.Event) {
 		i.bridge.discordMessagesChan <- IRCMessage{

commit 6078b3789733921f28da061fae561d12c229b58e

ircf: fix broken markdown caused by b3f18738c351c9bc3a217c6989d1f6a4d93686ec
block lists that end with a block that contains format didn't close its
stuff

irc/format/markdown.go
@@ -5,8 +5,12 @@ package ircf
 func BlocksToMarkdown(blocks []Block) string {
 	mdText := ""
 
-	for i, block := range blocks {
+	for i := 0; i < len(blocks)+1; i++ {
 		// Default to unstyled blocks when index out of range
+		block := Empty
+		if i < len(blocks) {
+			block = blocks[i]
+		}
 		prevBlock := Empty
 		if i > 0 {
 			prevBlock = blocks[i-1]

irc/format/format_test.go
@@ -34,6 +34,31 @@ func TestAllBlocks(t *testing.T) {
 	assert.Equal(t, expected, Parse(msgWithColor))
 }
 
+func TestFormatSimple(t *testing.T) {
+	cases := []struct {
+		Message  string
+		Input    string
+		Expected string
+	}{
+		{"spoilers", "\x0306,06text\x03", "||text||"},
+
+		// Test against https://github.com/reactiflux/discord-irc/blob/41f8444d17b2b282c437b5f871f0252968c03525/test/formatting.test.js
+		{"bold", "\x02text\x02", "**text**"},
+		{"reverse", "\x16text\x16", "*text*"},
+		{"italics", "\x1dtext\x1d", "*text*"},
+		{"underline", "\x1ftext\x1f", "__text__"},
+		{"color", "\x0306,08text\x03", "text"},
+		{"bold with nested italics", "\x02bold \x16italics\x16\x02", "**bold *italics***"},
+		{"bold with nested underline", "\x02bold \x1funderline\x1f\x02", "**bold __underline__**"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Message, func(t *testing.T) {
+			assert.Equal(t, c.Expected, BlocksToMarkdown(Parse(c.Input)))
+		})
+	}
+}
+
 func TestMarkdown(t *testing.T) {
 	msgMarkdown := `Hello, **Wor*ld*! *My name is***... __First__ Last. Testing reset***__ONETWO__***THREE. And *reverse*!`
 	assert.Equal(t,