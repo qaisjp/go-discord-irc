080fd878ce43a7d1caafd794ee991d1cc1a78520
Qais Patankar <qaisjp@gmail.com>
2021-02-21 14:34:30 +0000

    varys: pass lint

--- irc/varys/server.go
+++ irc/varys/server.go
@@ -1,19 +1,12 @@
 package varys
 
-import (
-	"log"
-	"net"
-	"net/http"
-	"net/rpc"
-)
-
 func NewServer() {
-	varys := new(Varys)
-	rpc.Register(varys)
-	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":1234")
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	go http.Serve(l, nil)
+	// varys := new(Varys)
+	// rpc.Register(varys)
+	// rpc.HandleHTTP()
+	// l, e := net.Listen("tcp", ":1234")
+	// if e != nil {
+	// 	log.Fatal("listen error:", e)
+	// }
+	// go http.Serve(l, nil)
 }
