737fac1fc2f061b58ae3b58bdb5db5110764f47d
Qais Patankar <qaisjp@gmail.com>
2021-03-08 23:38:14 +0000

    remove deadcode

--- dstate/dstate.go
+++ dstate/dstate.go
@@ -3,8 +3,6 @@ package dstate
 
 import "github.com/matterbridge/discordgo"
 
-var nilState = discordgo.ErrNilState
-
 func ChannelMessage(s *discordgo.Session, channelID string, messageID string) (*discordgo.Message, error) {
 	if msg, err := s.State.Message(channelID, messageID); err == nil {
 		return msg, err
